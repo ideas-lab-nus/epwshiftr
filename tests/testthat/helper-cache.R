8eeda1a7baf0e1870a77d0b9361e8f430b50cd61
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2021-11-22 16:54:04 +0800

    [check] Fix the error when getting cache dir

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -1,5 +1,5 @@
-get_cache <- function(path = Sys.getenv("EPWSHIFTR_CHECK_CACHE", NULL)) {
-    if (is.null(path)) path <- tools::R_user_dir("epwshiftr", "cache")
+get_cache <- function(path = Sys.getenv("EPWSHIFTR_CHECK_CACHE", NA)) {
+    if (is.na(path)) path <- tools::R_user_dir("epwshiftr", "cache")
 
     cache <- normalizePath(path, mustWork = FALSE)
     if (identical(Sys.getenv("NOT_CRAN"), "true") && !dir.exists(cache)) {
