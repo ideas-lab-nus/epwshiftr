873f12a6a656939ac2b55ec36a12fdfb4c7db477
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2026-06-24 08:43:18 +0800

    feat(query): add esgf query result core (#89)
    * feat(query): add esgf query result core
    
    * fix(query): stabilize core checks

---
+++ R/zzz.R
@@ -0,0 +1,26 @@
+# nocov start
+# reference: https://github.com/Rdatatable/data.table/blob/master/R/onLoad.R
+.onLoad <- function(libname, pkgname) {
+    # set package options
+    .opts <- list(
+        "epwshiftr.verbose" = FALSE,
+        "epwshiftr.threshold_alpha" = 3,
+        "epwshiftr.cache" = TRUE,
+        "epwshiftr.cache_dir" = tools::R_user_dir("epwshiftr", "cache")
+    )
+    missing <- setdiff(names(.opts), names(options()))
+    if (length(missing)) do.call(options, .opts[missing])
+
+    # install IEC style Byte units
+    IEC <- c("Byte", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+    big <- IEC[-1L]
+    small <- IEC[-length(IEC)]
+    for (i in seq_along(big)) {
+        try(units::install_unit(big[[i]], sprintf("1024 %s", small[[i]])),
+            silent = TRUE
+        )
+    }
+
+    invisible()
+}
+# nocov end
